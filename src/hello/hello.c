a4aa58b22fa574da94c3ee1318f3e6881b2e9e35
Douglas Smith <mcdoogen@gmail.com>
2024-06-10 21:55:03 -0400

    some notes on how to use the motor encoders

--- src/hello/hello.c
+++ src/hello/hello.c
@@ -50,16 +50,16 @@ int main() {
     gpio_set_function(PWMA_PIN, GPIO_FUNC_PWM);
     uint sliceA = pwm_gpio_to_slice_num(PWMA_PIN);
     uint chanA = pwm_gpio_to_channel(PWMA_PIN);
-    pwm_set_wrap(sliceA, 255);
-    pwm_set_chan_level(sliceA, chanA, 125);
+    pwm_set_wrap(sliceA, 100);    //MAX: 65535
+    pwm_set_chan_level(sliceA, chanA, 0);
     pwm_set_enabled(sliceA, true);
 
     const uint PWMB_PIN = 11;
     gpio_set_function(PWMB_PIN, GPIO_FUNC_PWM);
     uint sliceB = pwm_gpio_to_slice_num(PWMB_PIN);
     uint chanB = pwm_gpio_to_channel(PWMB_PIN);
-    pwm_set_wrap(sliceB, 255);
-    pwm_set_chan_level(sliceB, chanB, 125);
+    pwm_set_wrap(sliceB, 100);    //MAX: 65535
+    pwm_set_chan_level(sliceB, chanB, 0);
     pwm_set_enabled(sliceB, true);
 
     while (true) {
@@ -70,6 +70,21 @@ int main() {
         - Slow down to brake
         - Brake 3 seconds
         - Same but CCW
+
+
+        Is there any difference between stall and brake? And what if I set the PWM to zero?
+
+        //TODO:DS: How do I read the hall effect sensor?
+        - Interrupt on the rising edges?
+        - 90 degree phase difference?
+        - Go ahead and make this a ROS thing now
+        - Depending on the order of the signals tells you the direction it is spinning
+        - Each count of theedge pulse is a revolution
+        - Measure the time since the previous revolution, RPM
+        - Gear ratio is either 1:20 or... 244904:12000.... ?
+        - Look into C Structs to organize this better. And lets move some of this to headers to reuse on other projects! :D
+
+
     */
         // Stall 3 seconds
         gpio_put(LED_PIN, 0);
